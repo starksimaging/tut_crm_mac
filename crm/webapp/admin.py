86d952823d5c2a3be897f21f795ca5239427ef93
starksimaging <jonstarks6@gmail.com>
2025-03-02 10:26:04 -0500

    Models the Clients Model

--- crm/webapp/admin.py
+++ crm/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Client
 # Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email", "created_at")
+
+admin.site.register(Client)
--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(f"{self.full_name} {self.email} {self.created_at}")
